23c7a17257f9d5ff932414c98ac91087cbe56d62
laurencekenm-dev <laurencekenm@gmail.com>
2025-10-18 23:27:40 +0100

    Add files via upload

---
+++ kenm.py
@@ -0,0 +1,16 @@
+#enter your name
+print("what is your age")
+age =int(imput())
+print("what is your age")
+name =input()
+print("what is your age")
+age=int(imput))
+
+#enter your weight
+print ("what is your weight
+       weight=folat input())
+
+#enter your height
+print("what is your height:")
+height =folat(input)
+
